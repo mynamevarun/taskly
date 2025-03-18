bcf8cb86bdc8715ed4b13c00b6567871b8434aa3
Varun Kumar <varunguptaeng@gmail.com>
2025-03-18 23:24:27 +0530

    render screen as modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,17 @@ export default function IndexLayout() {
         name="index"
         options={{ title: "Shopping List" }}
       ></Stack.Screen>
+      <Stack.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          presentation: "modal",
+        }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="idea"
+        options={{ title: "Idea", presentation: "modal" }}
+      ></Stack.Screen>
     </Stack>
   );
 }
