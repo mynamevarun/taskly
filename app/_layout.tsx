2835c6c6db9df45dc2833d38341de14fd4a62e9d
Varun Kumar <varunguptaeng@gmail.com>
2025-03-18 23:43:14 +0530

    setup bottom tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,39 @@
 import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { themes } from "../themes";
 
 export default function IndexLayout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ tabBarActiveTintColor: themes.colorCerulean }}>
       <Tabs.Screen
         name="index"
-        options={{ title: "Shopping List" }}
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
       ></Tabs.Screen>
       <Tabs.Screen
         name="counter"
         options={{
           title: "Counter",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircleo" size={size} color={color} />
+          ),
         }}
       ></Tabs.Screen>
-      <Tabs.Screen name="idea" options={{ title: "Idea" }}></Tabs.Screen>
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
     </Tabs>
   );
 }
