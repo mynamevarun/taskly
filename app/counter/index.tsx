f51b3a0e5036697bad797056eda74309f7fba841
Varun Kumar <varunguptaeng@gmail.com>
2025-03-20 13:00:02 +0530

    added push notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,9 +1,41 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { registerForPushNotificationAsync } from "../../utils/registerForPushNotificationAsync";
+import { themes } from "../../themes";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
 export default function CounterScreen() {
+  const scheduleNotification = async () => {
+    const status = await registerForPushNotificationAsync();
+
+    if (status === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "I'm a notification from your app! 📨",
+        },
+        trigger: {
+          seconds: 5,
+        },
+      });
+    } else {
+      if (Device.isDevice) {
+        Alert.alert(
+          "Notification are not enabled for this app.",
+          "Enable the notifications from the settings."
+        );
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={scheduleNotification}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.text}>Schedule Notification</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,6 +48,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   text: {
-    fontSize: 24,
+    fontWeight: "bold",
+    color: themes.colorWhite,
+    letterSpacing: 1,
+    textTransform: "uppercase",
+  },
+  button: {
+    backgroundColor: themes.colorBlack,
+    padding: 12,
+    borderRadius: 6,
   },
 });
